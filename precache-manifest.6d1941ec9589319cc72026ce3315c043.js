715df638339589bc46f496e545ee85d67bee315e
SamsonChoo <hanye_choo@mymail.sutd.edu.sg>
2020-06-03 10:21:19 +0800

    Updates

---
+++ precache-manifest.6d1941ec9589319cc72026ce3315c043.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "702ebc42ed4e21ac3d5b083b7345e1db",
+    "url": "/Bienalle-Final/index.html"
+  },
+  {
+    "revision": "20bcc5d1c4311ac0c188",
+    "url": "/Bienalle-Final/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "68ad1deeeb0c5eebb5f1",
+    "url": "/Bienalle-Final/static/css/main.a2fea81b.chunk.css"
+  },
+  {
+    "revision": "20bcc5d1c4311ac0c188",
+    "url": "/Bienalle-Final/static/js/2.315ddaa3.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Bienalle-Final/static/js/2.315ddaa3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68ad1deeeb0c5eebb5f1",
+    "url": "/Bienalle-Final/static/js/main.377d6d59.chunk.js"
+  },
+  {
+    "revision": "8b6f766815dd4d38ade7",
+    "url": "/Bienalle-Final/static/js/runtime-main.fbb2ab42.js"
+  }
+]);
