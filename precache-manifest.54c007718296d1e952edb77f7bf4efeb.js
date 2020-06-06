a3f95c2020b9fee4910dafbcc1767de0183788a9
SamsonChoo <hanye_choo@mymail.sutd.edu.sg>
2020-06-06 17:20:40 +0800

    Updates

---
+++ precache-manifest.54c007718296d1e952edb77f7bf4efeb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "162efec48a62f7f050d172bfc60bb765",
+    "url": "/Bienalle-Final/index.html"
+  },
+  {
+    "revision": "56a930cc4182cb7d1752",
+    "url": "/Bienalle-Final/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "beec5dd5ccdfe52c206d",
+    "url": "/Bienalle-Final/static/css/main.8688932d.chunk.css"
+  },
+  {
+    "revision": "56a930cc4182cb7d1752",
+    "url": "/Bienalle-Final/static/js/2.3f8b6475.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Bienalle-Final/static/js/2.3f8b6475.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "beec5dd5ccdfe52c206d",
+    "url": "/Bienalle-Final/static/js/main.c1b077d7.chunk.js"
+  },
+  {
+    "revision": "8b6f766815dd4d38ade7",
+    "url": "/Bienalle-Final/static/js/runtime-main.fbb2ab42.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Bienalle-Final/precache-manifest.1803dab2e8a4a68d86b2aaca02afc1db.js"
+  "/Bienalle-Final/precache-manifest.54c007718296d1e952edb77f7bf4efeb.js"
 );
 
 self.addEventListener('message', (event) => {
