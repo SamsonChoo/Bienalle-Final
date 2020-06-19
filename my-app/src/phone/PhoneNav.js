408a63936e6c7aa6a956cdab736ed0637b8e1506
SamsonChoo <hanye_choo@mymail.sutd.edu.sg>
2020-06-19 22:35:45 +0800

    Changes for mobile

---
+++ my-app/src/phone/PhoneNav.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Line from '../svg/ThickLineShort';
+import './phone.css';
+
+export default class PhoneNav extends Component {
+  render() {
+    return (
+      <div>
+        <Line></Line>
+        <div class='phonec1'> Hybrid Highrise Commune</div>
+        <div class='phonec1'> Hybrid Highrise Commune</div>
+        <div class='phonec1'> Hybrid Highrise Commune</div>
+      </div>
+    );
+  }
+}
---
+++ my-app/src/svg/ThickLineShort.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+export default class ThickLineShort extends Component {
+  render() {
+    return (
+      <svg
+        width='197'
+        height='7'
+        viewBox='0 0 197 7'
+        fill='none'
+        xmlns='http://www.w3.org/2000/svg'
+      >
+        <line y1='3.5' x2='197' y2='3.5' stroke='white' stroke-width='7' />
+      </svg>
+    );
+  }
+}
